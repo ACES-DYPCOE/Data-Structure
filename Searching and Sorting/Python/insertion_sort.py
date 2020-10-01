38c5f20bd8cc419a98d9488fae27e5fb4ce55732
Yash Nitin Bhujbal <71966450+yashbhujbal1@users.noreply.github.com>
2020-10-01 17:39:27 +0530

    Add files via upload

---
+++ Searching and Sorting/Python/insertion_sort.py
@@ -0,0 +1,23 @@
+class Point:
+    def __init__(self, a):   #initialize the class definition
+        self.a = a
+
+
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]       #initialize temp to alist
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+
+
+alist = input('Enter the list of numbers: ').split()
+alist = [int(x) for x in alist]
+insertion_sort(alist)   #calling class definition
+for point in alist:
+    print('Sorted list: ', end='')
+    print(alist)
+
+#Time Complexity: O(n2)
