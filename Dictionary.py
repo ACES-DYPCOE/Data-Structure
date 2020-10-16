3f72a421cf9e461072d3589d8a37f2fb9bd35295
Dnyanada Dhore <71999647+DnyanadaD@users.noreply.github.com>
2020-10-16 22:15:19 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+my_dict = {'First': 'Python', 'Second': 'Java', 'Third': 'Ruby'}
+
+print(my_dict.keys())
+
+print(my_dict.values())
+
+print(my_dict.items())
+
+print(my_dict.get('First'))
+
+print(my_dict.get('Java'))
